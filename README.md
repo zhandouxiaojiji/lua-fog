86f2d3ecd2e7a59f7ed5a6d19cd2ec187939d574
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-08-22 18:50:44 +0800

    define api

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 地图迷雾压缩算法
-使用二叉树将庞大的地图迷雾数据压缩为较小的base64字符串，以便传输和存储。
+使用四叉树将庞大的地图迷雾数据压缩为较小的base64或者二进制字符串，以便传输和存储。
 
 # 区域迷雾状态
 |状态|标识|
@@ -9,44 +9,16 @@
 |全迷雾|2|
 
 # API
-+ fog.create(size) 创建指定大小的地图
-+ fog.encode(map) 序列化
-+ fog.decoce(str, size) 反序列化(返回新的map)
-+ fog.dispel(map, pos) 驱散迷雾
-+ fog.fog(map, pos) 设置迷雾
-+ fog.is_fog(map, pos) 检查迷雾
++ fog.create(w, h, tag) 创建指定大小的地图
++ fog.encode_base64(map) 序列化base64
++ fog.decode_base64(str, w, h) 反序列化base64(返回新的map)
++ fog.encode_binary(map) 序列化二进制字符串
++ fog.decode_binary(str, w, h) 序列化二进制字符串(返回新的map)
++ fog.dispel_fog(map, x, y, w, h) 驱散迷雾
++ fog.cover_fog(map, x, y, w, h) 覆盖迷雾
++ fog.is_fog(map, x, y) 检查迷雾
 + fog.union(map1, map2) 求并集
 
 # Usage
 ```lua
-local fog = require "fog"
-
-local size = 100000
-local map = fog.create(size)
-
-local str = fog.encode(map)
-print(str) -- ouput: B
-
-print("dispel 100 - 102")
-fog.dispel(map, 100)
-fog.dispel(map, 101)
-fog.dispel(map, 102)
-
-assert(not fog.is_fog(map, 100))
-assert(not fog.is_fog(map, 101))
-assert(not fog.is_fog(map, 102))
-
-str = fog.encode(map)
-print(str) -- ouput: qqqmqaoUVVVVB
-
-local new_map = fog.decode(str, size)
-assert(not fog.is_fog(new_map, 100))
-assert(not fog.is_fog(new_map, 101))
-assert(not fog.is_fog(new_map, 102))
-
-fog.fog(map, 100)
-fog.fog(map, 101)
-fog.fog(map, 102)
-
-print(fog.encode(map))  -- ouput: B
 ```
--- test.lua
+++ test.lua
@@ -1,67 +1,11 @@
 
 local fog = require "fog"
 
-local size = 100000
-local map = fog.create(size)
+local rect = {
+    x = 0,
+    y = 0,
+    w = 1000,
+    h = 1000,
+}
 
-local str = fog.encode(map)
-print(str)
-
-print("dispel 100 - 102")
-fog.dispel(map, 100)
-fog.dispel(map, 101)
-fog.dispel(map, 102)
-
-assert(not fog.is_fog(map, 100))
-assert(not fog.is_fog(map, 101))
-assert(not fog.is_fog(map, 102))
-
-str = fog.encode(map)
-print(str)
-local new_map = fog.decode(str, size)
-assert(not fog.is_fog(new_map, 100))
-assert(not fog.is_fog(new_map, 101))
-assert(not fog.is_fog(new_map, 102))
-
-fog.fog(map, 100)
-fog.fog(map, 101)
-fog.fog(map, 102)
-
-print(fog.encode(map))
-
-print("========= test union ==========")
-size = 10000
-local map1 = fog.create(size)
-fog.dispel(map1, 0)
-fog.dispel(map1, 2)
-
-local map2 = fog.create(size)
-fog.dispel(map2, 1)
-
-local map3 = fog.union(map1, map2)
-print(fog.encode(map1))
-print(fog.encode(map2))
-print(fog.encode(map3))
-
-assert(fog.is_dispel(map3, 0))
-assert(fog.is_dispel(map3, 1))
-assert(fog.is_dispel(map3, 2))
-
-print("========= test create ==========")
-local map4 = fog.create(size, fog.DISPEL)
-print(fog.encode(map4))
-
-print("========= test cmp ==========")
--- size = 3
-local map5 = fog.create(size)
-fog.dispel(map5, 0)
-fog.dispel(map5, 2)
-fog.dispel(map5, 3)
-
-local map6 = fog.create(size)
-fog.dispel(map6, 4)
-fog.dispel(map6, 2)
-fog.dispel(map6, 3)
-local new_fog_list, new_dispel_list = fog.cmp(map5, map6)
-print("new_fog_list", table.concat(new_fog_list, ","))
-print("new_dispel_list", table.concat(new_dispel_list, ","))
+local map = fog.create(rect.w, rect.h)
